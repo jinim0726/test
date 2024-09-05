6fb324a75735b2f94dd71fc3d4d4925a3d6aa3e5
jinim0726/E232 <yangmin0726@gmail.com>
2024-09-05 13:17:47 +0900

    0905 1교시

---
+++ 0905/save.cpp
@@ -0,0 +1,39 @@
+//----------------------------------------------
+// save.cpp - 한 학기 강의를 저장하기 위한 기능 모음
+// 2024.09.05
+//----------------------------------------------
+#include "save.h"
+#include <fstream>
+#include <chrono>
+#include <iostream>
+
+void save(const std::string& FileName)
+{
+	std::ifstream in{ FileName };
+	if (not in)
+	{
+		return;
+	}
+
+	else
+	{
+		std::ofstream out{ "2024 2 C++ 월78목45.txt", std::ios::app };
+
+		auto now = std::chrono::system_clock::now();
+		using namespace std::literals;
+		now += 9h;
+
+		out << std::endl;
+		out << "=========================" << std::endl;
+		//out << "저장시간 : " << now << std::endl;
+		out << "=========================" << std::endl;
+		out << std::endl;
+
+		char c;
+		in >> std::noskipws;
+		while (in >> c)
+		{
+			out << c;
+		}
+	}
+}
